b364bef5f5aebc83e04ab92287ddfb37d14e09a1
Igor <igor.kiryanov@masterofcode.com>
2018-11-23 22:44:05 +0200

    Chatbot init

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const request = require('request');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+mongoose.connect(`mongodb://${process.env.MONGO_ATLAS_USR}:${process.env.MONGO_ATLAS_PWD}@nodechatbottest-shard-00-00-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-01-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-02-s4bul.azure.mongodb.net:27017/test?ssl=true&replicaSet=NodeChatbotTest-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true });
+// mongoose.connect('mongodb+srv://admin:admin@nodechatbottest-s4bul.azure.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
 app.set('port', process.env.PORT || 3000);
 
@@ -14,12 +17,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook/', (req, res) => {
-    if (req.query["hub.verify_token"] === "test"){
-        return res.send(req.query["hub.challenge"]);
-    }
-    res.send("Wrong token!");
+  if (req.query['hub.verify_token'] === 'test') {
+    return res.send(req.query['hub.challenge']);
+  }
+  return res.send('Wrong token!');
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Listening...');
+  console.log('Listening...');
 });
