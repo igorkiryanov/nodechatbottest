1c7a793e4bc343e935624f5b016e222698db24d3
Igor <igor.kiryanov@masterofcode.com>
2018-11-23 23:15:42 +0200

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 mongoose.connect(`mongodb://${process.env.MONGO_ATLAS_USR}:${process.env.MONGO_ATLAS_PWD}@nodechatbottest-shard-00-00-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-01-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-02-s4bul.azure.mongodb.net:27017/test?ssl=true&replicaSet=NodeChatbotTest-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true });
 // mongoose.connect('mongodb+srv://admin:admin@nodechatbottest-s4bul.azure.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
