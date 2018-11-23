52e1e2cbd681d6f9f9d622117fddc249c1dadd60
Igor <igor.kiryanov@masterofcode.com>
2018-11-24 00:04:08 +0200

    test answer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -7,7 +8,7 @@ const app = express();
 mongoose.connect(`mongodb://${process.env.MONGO_ATLAS_USR}:${process.env.MONGO_ATLAS_PWD}@nodechatbottest-shard-00-00-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-01-s4bul.azure.mongodb.net:27017,nodechatbottest-shard-00-02-s4bul.azure.mongodb.net:27017/test?ssl=true&replicaSet=NodeChatbotTest-shard-0&authSource=admin&retryWrites=true`, { useNewUrlParser: true });
 // mongoose.connect('mongodb+srv://admin:admin@nodechatbottest-s4bul.azure.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || process.env.LOCAL_PORT);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,10 +18,35 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook/', (req, res) => {
-  if (req.query['hub.verify_token'] === 'test') {
+  if (req.query['hub.verify_token'] === process.env.FB_SECRET) {
     return res.send(req.query['hub.challenge']);
   }
   return res.send('Wrong token!');
+});
+
+function sendText(sender, text) {
+  const messageData = { text };
+  request({
+    url: process.env.FB_MESSAGE_URL,
+    qs: { access_token: process.env.FB_TOKEN },
+    method: 'POST',
+    json: {
+      recipient: { id: sender },
+      message: messageData,
+    },
+  });
+}
+
+app.post('/webhook/', (req, res) => {
+  const messagingEvents = req.body.entry[0].messaging;
+  for (let i = 0; i < messagingEvents.lenth; i += 1) {
+    const event = messagingEvents[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      sendText(sender, `Echo text: ${event.message.text}`);
+    }
+  }
+  res.status(200);
 });
 
 app.listen(app.get('port'), () => {
