1179d2d1798333dfe0a9a4fc4d3d45e49fa994b1
Igor <igor.kiryanov@masterofcode.com>
2018-11-23 15:50:50 +0200

    eslint init

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send("Hi, I'm chatbot!");
+  res.send("Hi, I'm chatbot!");
 });
 
 app.get('/webhook/', (req, res) => {
-    if (req.query['hub.verify_token'] === 'test') {
-        return res.send(req.query['hub.challenge']);
+    if (req.query["hub.verify_token"] === "test"){
+        return res.send(req.query["hub.challenge"]);
     }
-    res.send("Wrong token");
+    res.send("Wrong token!");
 });
 
 app.listen(app.get('port'), () => {
